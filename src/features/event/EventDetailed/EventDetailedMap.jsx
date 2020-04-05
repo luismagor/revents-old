bf49a1d878a0f6102042c5f415cc99ea67afc140
Luis M. García <luismago@gmail.com>
2020-04-05 13:34:38 +0200

    rename API key environment var

--- src/features/event/EventDetailed/EventDetailedMap.jsx
+++ src/features/event/EventDetailed/EventDetailedMap.jsx
@@ -12,7 +12,7 @@ const EventDetailedMap = ({ lat, lng }) => {
     <Segment attached="bottom" style={{ padding: 0 }}>
       <div style={{ height: '300px', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
           defaultCenter={{ lat, lng }}
           defaultZoom={zoom}
         >
--- src/features/testarea/SimpleMap.js
+++ src/features/testarea/SimpleMap.js
@@ -17,7 +17,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '300px', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
           defaultCenter={latlng}
           defaultZoom={this.props.zoom}
         >
